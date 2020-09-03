be7c40d296cad7ebaeb907be74fbdcce492f7b0d
Tyler Casperson <tylercasperson@gmail.com>
2020-09-03 12:36:28 -0600

    moved enviroment variables

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,24 @@
-var mysql = require("mysql");
+require('dotenv');
+var mysql = require('mysql');
 
-if (process.env.JAWSDB_URL){
-    var connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    var connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "root",
-        database: "burgerDB"
-    });
-}  
+  var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'root',
+    database: 'burgerDB',
+  });
+}
 
-connection.connect(function(err) {
-    if(err) {
-        console.error("error connection: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+connection.connect(function (err) {
+  if (err) {
+    console.error('error connection: ' + err.stack);
+    return;
+  }
+  console.log('connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
