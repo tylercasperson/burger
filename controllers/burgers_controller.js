5256d36809343103073299ec468c692163bce39a
Tyler Casperson <tylercasperson@gmail.com>
2020-09-04 23:29:19 -0600

    allows for a single item to be displayed on the screen

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,13 +7,12 @@ router.get('/', function (req, res) {
     var hbsObject = {
       burgers: data,
     };
-    // console.log(hbsObject);
     res.render('index', hbsObject);
   });
 });
 
 router.post('/api/burgers', function (req, res) {
-  if (req.body.length === undefined) {
+  if (req.body.burger_name === undefined) {
     burgers.insertOriginal(function (result) {
       res.redirect('/');
     });
@@ -25,11 +24,8 @@ router.post('/api/burgers', function (req, res) {
 });
 
 router.put('/burgers/:id', function (request, res) {
-  // var id = request.params.id;
-  // console.log(request);
   var condition = 'id = ' + request.params.id;
   burgers.updateOne(condition, function (result) {
-    // res.json(data);
     console.log(result);
     res.sendStatus(200);
   });
@@ -50,13 +46,6 @@ router.put('/api/burgers', function (req, res) {
 });
 
 router.delete('/api/burgers', function (req, res) {
-  burgers.deleteAll(function (result) {
-    console.log(result);
-    res.sendStatus(200);
-  });
-});
-
-router.delete('/burgers', function (request, res) {
   burgers.deleteAll(function (result) {
     console.log(result);
     res.sendStatus(200);
